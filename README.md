1d0b06e30f0e0368418e1232569a7ae6c2934c6f
Shamim Akhter <sa151097akhter@gmail.com>
2023-08-18 21:32:06 +0530

    API created

---
+++ README.md
@@ -0,0 +1,3 @@
+# Request Header Parser Microservice
+
+This Project is all about getting information of request object
---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app=express();
+require('dotenv').config();
+const cors=require('cors');
+app.use(cors({optionsSuccessStatus: 200}));
+
+app.use(express.static('public'));
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/views/index.html');
+  });
+
+  //
+app.get('/api/whoami',function(req,res){
+    const information = req.headers;
+    res.send({
+        "ipaddress":information.origin,
+        "language":information['accept-language'],
+        "software":information['user-agent']
+    });
+
+    console.log(information);
+});
+
+app.listen(process.env.PORT || 8000,function(){
+    console.log("Server is listening on port 8000");
+})
