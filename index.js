93d9b80e790e6fbe78693a5d80f04659131398b0
Shamim Akhter <sa151097akhter@gmail.com>
2023-08-18 23:58:06 +0530

    trust proxy enabled in index.js

--- index.js
+++ index.js
@@ -5,12 +5,12 @@ const cors=require('cors');
 app.use(cors({optionsSuccessStatus: 200}));
 
 app.use(express.static('public'));
-
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/views/index.html');
   });
 
-  //
+
+app.enable('trust proxy');
 app.get('/api/whoami',function(req,res){
     const information = req.headers;
     res.send({
@@ -18,6 +18,7 @@ app.get('/api/whoami',function(req,res){
         "language":information['accept-language'],
         "software":information['user-agent']
     });
+    console.log(information);
 });
 
 app.listen(process.env.PORT || 8000,function(){
